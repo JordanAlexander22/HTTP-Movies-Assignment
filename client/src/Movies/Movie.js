75307d731b04064b4ae95ffc5a2f4adc93db9b79
jordan <jordan@jordans-Air.lan>
2019-12-18 22:26:25 +0700

    made mvp,was able to get delete and put working

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,72 +1,62 @@
-import React, { Component } from "react";
-import axios from "axios";
-class CreateMovie extends Component {
-  state = {
-    title: "",
-    director: "",
-    metaScore: 0,
-    actor: "",
-    stars: []
-  };
-  handleAddstars = () => {
-    const { stars } = this.state;
-    stars.push(this.state.actor);
-    this.setState({ actor: "", stars });
-  };
-  handleTextInput = event => {
-    console.log(event.target.value);
-    this.setState({ [event.target.name]: event.target.value });
-  };
-  handleSubmitMovie = () => {
-    const { stars, title, metaScore, director } = this.state;
-    const newMovie = { stars, title, metaScore, director };
-    axios
-      .post("http://localhost:3333/api/movies", newMovie)
-      .then(response => {
-        this.props.history.push("/");
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
-  render() {
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="Movie Title"
-          value={this.state.title}
-          onChange={this.handleTextInput}
-          name="title"
-        />
-        <input
-          type="text"
-          placeholder="Director"
-          value={this.state.director}
-          onChange={this.handleTextInput}
-          name="director"
-        />
-        <input
-          type="text"
-          placeholder="Meta Score"
-          value={this.state.metaScore}
-          onChange={this.handleTextInput}
-          name="metaScore"
-        />
-        <input
-          type="text"
-          placeholder="..Add actor"
-          value={this.state.actor}
-          onChange={this.handleTextInput}
-          name="actor"
-        />
-        <button onClick={this.handleAddstars}>Add Actor to List</button>
-        <button onClick={this.handleSubmitMovie}>Save Movie</button>
-        {this.state.stars.map(actor => {
-          return <div>{actor}</div>;
-        })}
-      </div>
-    );
-  }
+import React from 'react';
+import axios from 'axios';
+import MovieCard from './MovieCard';
+export default class Movie extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			movie: null
+		};
+	}
+
+	componentDidMount() {
+		this.fetchMovie(this.props.match.params.id);
+	}
+
+	componentWillReceiveProps(newProps) {
+		if (this.props.match.params.id !== newProps.match.params.id) {
+			this.fetchMovie(newProps.match.params.id);
+		}
+	}
+
+	fetchMovie = (id) => {
+		axios
+			.get(`http://localhost:5000/api/movies/${id}`)
+			.then((res) => this.setState({ movie: res.data }))
+			.catch((err) => console.log(err.response));
+	};
+
+	saveMovie = () => {
+		const addToSavedList = this.props.addToSavedList;
+		addToSavedList(this.state.movie);
+	};
+
+	deleteMovie = (event) => {
+		event.preventDefault();
+		axios
+			.delete(`http://localhost:5000/api/movies/${this.props.match.params.id}`)
+			.then((response) => {
+				this.props.history.push('/');
+			})
+			.catch((err) => console.log(err.response));
+	};
+
+	render() {
+		if (!this.state.movie) {
+			return <div>Loading movie information...</div>;
+		}
+
+		return (
+			<div className="save-wrapper">
+				<MovieCard movie={this.state.movie} />
+				<div className="save-button" onClick={this.saveMovie}>
+					Save
+				</div>
+				<button onClick={() => this.props.history.push(`/edit-movie/${this.state.movie.id}`)}>
+					Edit Movie
+				</button>
+				<button onClick={this.deleteMovie}>Delete movie</button>
+			</div>
+		);
+	}
 }
-export default CreateMovie;
